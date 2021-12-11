903e86b77fd53a67d98773980ab4f006956db595
LyonEscalli <88354028+LyonEscalli@users.noreply.github.com>
2021-12-10 22:52:58 -0300

    Adicionei exercício 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -79,7 +79,11 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+      let anonimo = {
+    ...pessoa,
+    nome: "ANÔNIMO"
+    }
+return anonimo
 }
 
 // EXERCÍCIO 13A
