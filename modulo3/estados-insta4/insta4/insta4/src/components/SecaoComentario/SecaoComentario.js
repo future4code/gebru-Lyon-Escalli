1ae3acfac429947ee6509fcf7624178925f1d811
LyonEscalli <lyonescalli@gmail.com>
2022-02-01 21:41:47 -0300

    Adicionei o exercício estados-insta

---
+++ modulo3/estados-insta4/insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+
+export class SecaoComentario extends Component {
+	state = {
+		valorComentario: ''
+	}
+
+	onChangeComentario = (event) => {
+		this.setState({
+			valorComentario: event.target.value,
+		})
+		console.log(this.state.valorComentario)
+	}
+
+	render() {
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				onChange={this.onChangeComentario}
+				value={this.state.valorComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+	}
+}
