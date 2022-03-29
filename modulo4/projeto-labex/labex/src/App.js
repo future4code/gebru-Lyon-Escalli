4bee4e2f9cda5a31ca4cf563a64a420699bfac0e
LyonEscalli <lyonescalli@gmail.com>
2022-03-29 20:10:57 -0300

    Atualização 1 - entrega do wireframes e esqueleto react

---
+++ modulo4/projeto-labex/labex/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Olá!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo4/projeto-labex/labex/src/pages/HomePage.js
@@ -0,0 +1,1 @@
+//Para o usuário escolher entre Área Administrativa e Lista de Viagens
