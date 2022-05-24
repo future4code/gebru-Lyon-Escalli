1ca21b1357ce93f4a987c607d2bab3d182e17ee8
LyonEscalli <lyonescalli@gmail.com>
2022-05-24 15:40:10 -0300

    Exercicio TS 2

---
+++ modulo5/intro-typescript/template-APIs-REST/src/exercicio01.ts
@@ -0,0 +1,9 @@
+export const testarTriangulo = (a:number, b:number, c:number):string => {
+    if (a !== b && b !== c) {
+        return "Escaleno";
+      } else if (a === b && b === c) {
+        return "Equilátero";
+      } else {
+        return "Isósceles";
+    }
+}
---
+++ modulo5/intro-typescript/template-APIs-REST/src/index.ts
@@ -0,0 +1,15 @@
+import { testarTriangulo } from "./exercicio01"
+import { imprimeTresCoresFavoritas } from './exercicio02'
+import { checaAnoBissexto } from './exercicio03'
+import { comparaDoisNumeros } from './exercicio04'
+import { checaRenovacaoRG } from './exercicio05'
+
+
+testarTriangulo(10,10,10)
+imprimeTresCoresFavoritas()
+checaAnoBissexto(2000)
+comparaDoisNumeros(10,5)
+checaRenovacaoRG(2022,1994,2020)
+
+
+
