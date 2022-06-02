24e41aaabe25b0d4639326817f9d851658e01e8b
LyonEscalli <lyonescalli@gmail.com>
2022-06-02 19:37:16 -0300

    Exercício aprofundamento express

---
+++ modulo5/aprofundamento-express/template-aula-express/src/dataTarefas.ts
@@ -0,0 +1,56 @@
+export const dataTarefas = [
+    {
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "quis ut nam facilis et officia qui",
+      completed: true
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "fugiat veniam minus",
+      completed: false
+    },
+    {
+        userId: 2,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: true
+      },
+      {
+        userId: 2,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false
+      },
+      {
+        userId: 3,
+        id: 1,
+        title: "delectus aut autem",
+        completed: true
+      },
+      {
+        userId: 3,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: true
+      },
+      {
+        userId: 3,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false
+      }
+  ]
